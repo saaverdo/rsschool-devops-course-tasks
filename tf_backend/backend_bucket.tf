b97d51f49970141f4fa66544a3356e5222f60e3e
serg <saaverdo@gmail.com>
2025-06-09 19:31:31 +0000

    Initial s3 backend configuration added

---
+++ tf_backend/backend_bucket.tf
@@ -0,0 +1,34 @@
+resource "aws_s3_bucket" "backend" {
+  bucket = "rs-devops-tf-state-bucket"
+  tags = {
+    Name        = "TF state bucket"
+    Project = "Devops"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning_backend" {
+    bucket = aws_s3_bucket.backend.id
+    versioning_configuration {
+      status = "Enabled"
+    }
+}
+
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name = "rs-devops-tf-state-lock"
+  hash_key = "LockID"
+  read_capacity = 1
+  write_capacity = 1
+ 
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+output "state_bucket" {
+  value = aws_s3_bucket.backend.bucket
+}
+
+output "state_lock_table" {
+  value = aws_dynamodb_table.dynamodb-terraform-state-lock.name
+}
