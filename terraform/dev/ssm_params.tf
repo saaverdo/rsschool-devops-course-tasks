8e345f7ae90763328af29a19b162d54265a2b5c5
serg <saaverdo@gmail.com>
2025-06-29 00:40:39 +0000

    fix ssm params

--- terraform/dev/ssm_params.tf
+++ terraform/dev/ssm_params.tf
@@ -3,3 +3,21 @@ resource "aws_ssm_parameter" "k3s_master_ip" {
   type  = "String"
   value = local.k3s_master_ip
 }
+
+resource "aws_ssm_parameter" "k3s_config" {
+  name  = "/dev/k3s/config"
+  type  = "SecureString"
+  value = ""
+  lifecycle {
+    ignore_changes = [value,key_id]
+  }
+}
+
+resource "aws_ssm_parameter" "k3s_token" {
+  name  = "/dev/k3s/node-token"
+  type  = "SecureString"
+  value = ""
+  lifecycle {
+    ignore_changes = [value,key_id]
+  }
+}
